b4282c81b4fa8d7011dd7791f6f1308078da67e3
magnas10 <sangamdevkota14@gmail.com>
2025-12-05 20:18:21 +1100

    fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+// ScrollToTop component - scrolls to top on route change
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 import AdminRoute from './components/AdminRoute';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
@@ -53,6 +64,7 @@ function App() {
   return (
     <AuthProvider>
       <Router>
+        <ScrollToTop />
         <div className="App">
           <Routes>
             {/* Admin Routes - No Header/Footer */}
